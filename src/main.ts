eb19fbae923b13b737d88477f768eac61b4c1113
Ad0lph <your.email@example.com>
2026-05-23 03:20:59 +0400

    Changes for production

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.set('trust proxy', 1);
   app.enableShutdownHooks();
 
   app.enableCors({
